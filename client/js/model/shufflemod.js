07f9a702c4f6c708bdc5341fc23442c367b87805
jubianchi <contact@jubianchi.fr>
2012-11-25 03:11:36 +0100

    Add missing file

---
+++ client/js/model/shufflemod.js
@@ -0,0 +1,38 @@
+define([
+  'underscore',
+  'model/mod'
+], function(_, Mod){
+
+  // Model for input
+  var Shuffle = function() {
+    Mod.apply(this, arguments);
+  };
+
+  _.extend(Shuffle.prototype, Mod.prototype, {
+
+    process:function(text, position) {
+      position++;
+
+      while(text.substring(position, position + 1) != ' ') position++;
+      position++;
+
+      var words = text.substring(position).match(/(\w*) (\w*)/);
+      return text.substring(0, position) + text.substring(position).replace(words[1] + ' ' + words[2], this.shuffle(words[1]) + ' ' + this.shuffle(words[2]));
+    },
+
+    shuffle: function(s) {
+      var a = s.split(""),
+          n = a.length;
+
+      for(var i = n - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var tmp = a[i];
+          a[i] = a[j];
+          a[j] = tmp;
+      }
+      return a.join("");
+    }
+  });
+
+  return Shuffle;
+});
