c55825807aa07bf480e2d7fe6a601dd13d591439
Feugy <damien.feugas@gmail.com>
2012-11-25 16:16:27 +0100

    Home page

--- client/nls/fr/common.js
+++ client/nls/fr/common.js
@@ -55,7 +55,10 @@ define({
     ],
     ends2: [
       "ceci est l'autre phrase de fin"
-    ]
+    ],
+    pitch: "<p>Lors d'une expédition dans des vestiges mayas, le jeune archéologue Adam découvrit une étrange tablette de pierre vierge. Sans intérêt apparent, il en fit une table basse.</p>"+
+      "<p>Mais le 21 Décembre 2012, elle se mit à briller et une voix céleste se fit entendre :<b><i>\"Humain, si vous souhaitez sauver l'humanité, il vous faudra écrire l'histoire... plus vite que moi !\"</i></b></p>",
+      controls: "<p>Comment jouer ?</p><p><b>TABULAS</b> est une course de saisie.</p><p>Vous gravez à l'aide de votre clavier et déclencher vos malus avec <b>Ctrl</b></p>"
   },
 
   tips: {
