a251efc52613341200e7e87cc90c06fe61b84769
feugy <pioupiou@gmail.com>
2012-11-27 23:09:04 +0100

    Add disclaimer and GameDevLogo

--- client/nls/fr/common.js
+++ client/nls/fr/common.js
@@ -28,16 +28,18 @@ define({
     pitch: "<p>Lors d'une expédition dans des vestiges mayas, le jeune archéologue Adam découvrit une étrange tablette de pierre vierge. Sans intérêt apparent, il en fit une table basse.</p>" +
            "<p>Mais le 21 Décembre 2012, elle se mit à briller et une voix céleste se fit entendre :<b><i>\"Humain, si vous souhaitez sauver l'humanité, il vous faudra écrire l'histoire... plus vite que moi !\"</i></b></p>",
     controls: "<p>Comment jouer ?</p><p><b>TABULAS</b> est une course de saisie.</p><p>Vous gravez à l'aide de votre clavier et déclenchez vos malus avec <b>Ctrl</b></p>",
-    credits: '<h3>maxime payot</h3>' +
-             '<p>game/sound designer&ensp;&ensp;//&ensp;&ensp;musiques, bruitages, pitch, textes</p>' + 
+    credits: '<a href="http://gamedevparty.fr/" target="blank"><img src="/img/logo-gamedevparty.png"/></a>' +
+             '<p class="disclaimer">Tabulas est un jeu conçu et développé en 32h lors de la <a target="blank" href="http://gamedevparty.fr/gdpjam3-le-debrief/">GamedDevParty Jam #3</a> par cinq amateurs passionés:</p>' +
+             '<h3>maxime payot</h3>' +
+             '<p>Game/sound designer&ensp;&ensp;//&ensp;&ensp;Musiques, Bruitages, Pitch, Textes</p>' + 
              '<h3><a href="https://twitter.com/CharlesPayot" target="_blank">charles payot</a></h3>' +
-             '<p>graphiste&ensp;&ensp;//&ensp;&ensp;ui, items</p>' +
+             '<p>Graphiste&ensp;&ensp;//&ensp;&ensp;UI, Items</p>' +
              '<h3><a href="https://twitter.com/eGwan" target="_blank">gwendoline ancelin</a></h3>' +
-             '<p>graphiste&ensp;&ensp;//&ensp;&ensp;decors, personnages</p>' +
+             '<p>Graphiste&ensp;&ensp;//&ensp;&ensp;Décors, Personnages</p>' +
              '<h3><a href="https://plus.google.com/u/0/104534045605473633165/posts" target="_blank">damien Feugas</a></h3>' +
-             '<p>developpeur&ensp;&ensp;//&ensp;&ensp;archi, multijoueur, sons</p>' +
+             '<p>Développeur&ensp;&ensp;//&ensp;&ensp;Archi, Multijoueur, Sons</p>' +
              '<h3><a href="https://twitter.com/jubianchi" target="_blank">julien bianchi</a></h3>' +
-             '<p>developpeur&ensp;&ensp;//&ensp;&ensp;ia, items, equilibrage</p>' +
+             '<p>Développeur&ensp;&ensp;//&ensp;&ensp;IA, Items, Equilibrage</p>' +
              '<br/><h3><a href="https://github.com/jubianchi/gdpjam3" target="_blank">sources // github</a><h3>'
 
   },
